9c44c83b61872d230b47477054a42a20cc8b159c
Nothing <113851050+luanndm-dream@users.noreply.github.com>
2024-12-03 00:42:05 +0700

    Hidden firebase key

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  databaseURL: "https://fbus-public-map-default-rtdb.asia-southeast1.firebasedatabase.app"
+ **************************************
   //"https://f-bus-map-default-rtdb.firebaseio.com",
 };
 
